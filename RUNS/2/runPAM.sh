38db4379f0dcfd467db2ac10b5aa066f2a600786
XinyunLiuX <166228187+XinyunLiuX@users.noreply.github.com>
2025-08-16 14:48:43 -0400

    add .sh file to 123

--- RUNS/2/runPAM.sh
+++ RUNS/2/runPAM.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 #SBATCH -p general
-#SBATCH -N 1
-#SBATCH -n 20
+#SBATCH --nodes=1
+#SBATCH --ntasks=20
+#SBATCH --cpus-per-task=1
 #SBATCH --mem=64g
 #SBATCH -t 07-00:00:00
 
-module load matlab/2023b
-matlab -nodesktop -nosplash -singleCompThread -r runner_postprocessing -logfile PAM_modulation2.out
+module load matlab/2024b
+matlab -nodesktop -nosplash -singleCompThread -r runner_postprocessing -logfile PAM.out
 
 
